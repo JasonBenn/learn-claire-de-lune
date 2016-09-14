13456b82e17afd8001a007b46683566d65196062
Jason Benn <jasoncbenn@gmail.com>
2016-09-13 22:15:19 -0700

    Update urls.py.

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,16 +1,13 @@
 from django.conf.urls import include, url
 
 from django.contrib import admin
+
+from piano import views
+
 admin.autodiscover()
 
-# import hello.views
-
-# Examples:
-# url(r'^$', 'gettingstarted.views.home', name='home'),
-# url(r'^blog/', include('blog.urls')),
-
 urlpatterns = [
-    # url(r'^$', hello.views.index, name='index'),
-    # url(r'^db', hello.views.db, name='db'),
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index'),
+    url(r'^api/performances/', views.performances),
+    url(r'^admin/', include(admin.site.urls))
 ]
