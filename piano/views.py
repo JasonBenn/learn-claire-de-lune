288db07fe00f126139aac6fe85443fba506ff58f
Jason Benn <jasoncbenn@gmail.com>
2016-09-13 22:15:32 -0700

    Basic home view.

--- piano/views.py
+++ piano/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from piano.models import Bookmark, Performance
+
+
+def index(request):
+    bookmarks = Bookmark.objects.all()
+    return render(request, 'index.html', {
+        'bookmarks': bookmarks
+    })
+
 
 # Create your views here.
