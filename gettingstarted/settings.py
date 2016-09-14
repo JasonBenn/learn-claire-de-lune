b33d3ed0ab85d1af6a7311c36d88620430877879
Jason Benn <jasoncbenn@gmail.com>
2016-09-13 21:08:28 -0700

    First pass at models/migrations.

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -37,14 +37,14 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello'
+    'piano'
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.authu.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
---
+++ piano/models.py
@@ -0,0 +1,20 @@
+from __future__ import unicode_literals
+
+from django.core.validators import validate_comma_separated_integer_list
+from django.db import models
+
+
+class Performance(models.Model):
+    start_location = models.BigIntegerField(db_index=True)
+    end_location = models.BigIntegerField(db_index=True)
+    total_seconds = models.SmallIntegerField()
+    mistakes = models.TextField(validators=[validate_comma_separated_integer_list])
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+
+class Bookmark(models.Model):
+    start_location = models.BigIntegerField(db_index=True)
+    end_location = models.BigIntegerField(db_index=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
