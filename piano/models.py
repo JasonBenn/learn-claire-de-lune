e54b1999be32023507e24c7ac04dec4c4d8e9f99
Jason Benn <jasoncbenn@gmail.com>
2016-09-17 23:05:32 -0700

    Add bookmarks and static assets to index.html.

--- piano/models.py
+++ piano/models.py
@@ -8,13 +8,13 @@ class Performance(models.Model):
     start_location = models.BigIntegerField(db_index=True)
     end_location = models.BigIntegerField(db_index=True)
     total_seconds = models.SmallIntegerField()
-    mistakes = models.TextField(validators=[validate_comma_separated_integer_list])
+    mistakes = models.TextField(validators=[validate_comma_separated_integer_list], null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
 
 class Bookmark(models.Model):
     start_location = models.BigIntegerField(db_index=True)
-    end_location = models.BigIntegerField(db_index=True)
+    end_location = models.BigIntegerField(db_index=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
